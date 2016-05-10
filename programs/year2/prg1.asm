a9f85725a0044bf70d937b35e1270ecce030201c
Balazs Pocze <banyek@kinja.com>
2016-05-10 09:34:38 +0200

    statistics removed

--- programs/year2/prg1.asm
+++ programs/year2/prg1.asm
@@ -1,6 +1,4 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
--- Size 3/3  PASS   --
--- Speed 30/25      --
 
 a:
     INBOX   
--- programs/year2/prg2.asm
+++ programs/year2/prg2.asm
@@ -1,7 +1,4 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
--- Size  24/3    --
--- Speed 20/25   --
-
 
     INBOX   
     OUTBOX  
