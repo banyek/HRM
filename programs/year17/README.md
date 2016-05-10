293d01fde29ab2cc59e3a96e1998c23e5fe6a288
Balazs Pocze <banyek@kinja.com>
2016-05-10 17:02:33 +0200

    year 17

---
+++ programs/year17/README.md
@@ -0,0 +1,18 @@
+# Exclusive Lounge
+
+For each TWO things in the INBOX:
+
+Send a 0 to the OUTBOX if they have the same sign. (Both positive or both negative.)
+
+Send a 1 to the OUTBOX if their signs are different.
+Repeat until the INBOX is empty.
+
+###Challenges:
+- Size:  12
+- Speed: 12
+
+###Programs:
+
+|NAME|SIZE|SPEED|COMMENT|
+| --- | --- | --- | --- |
+|prg1.asm| 14|30|
---
+++ programs/year17/prg.asm
@@ -0,0 +1,32 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+a:
+b:
+    INBOX   
+    JUMPN    c
+    INBOX   
+    JUMPN    g
+    JUMP     e
+c:
+    INBOX   
+    JUMPN    d
+    JUMP     f
+    COMMENT  0
+d:
+e:
+    COPYFROM 4 -- default 0
+    OUTBOX  
+    JUMP     b
+f:
+g:
+    COPYFROM 5 -- default 1
+    OUTBOX  
+    JUMP     a
+
+
+DEFINE COMMENT 0
+eJxTZ2Bg2Jz36jmQYuiRe6j5QK5iWphR+VQQv1FJNjRHSzYUwr6tulf7tmqw8QMNEL/IvXRKkftz3QKP
+F3o7fVhdXCMsM6dFepZMiwyoNI0rmdwZF914O94ysyXhg4lu4lMdkJ5fxY+0lhZXTAOxVxTVzeqoq5vV
+1FA6pbYpvUu5xa9CuUUjVq1ZLMCw/p/d1MqX+gyjYBSMApoDAFS8OHY;
+
+
