d099f3d615746ef93af9af11565d51290f48305c
Balazs Pocze <banyek@kinja.com>
2016-05-10 09:37:59 +0200

    Added comments

--- programs/year3/prg1.asm
+++ programs/year3/prg1.asm
@@ -2,11 +2,11 @@
 -- Size   6/6 --
 -- Speed  6/6 --
 
-    COPYFROM 4
+    COPYFROM 4  -- B 
     OUTBOX  
-    COPYFROM 0
+    COPYFROM 0  -- U
     OUTBOX  
-    COPYFROM 3
+    COPYFROM 3  -- G
     OUTBOX  
 
 
